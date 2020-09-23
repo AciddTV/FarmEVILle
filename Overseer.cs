29416f1ab8cb49b6097cd58743b33c9532bc23da
ChalkMagnetTV <69421081+ChalkMagnetTV@users.noreply.github.com>
2020-09-23 10:25:47 +0200

    Overseer will control UI and overall information
    Collects data from all available sources (factory buildings).

---
+++ Overseer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryEngine.Projects.Game
+{
+    public class Overseer
+    {
+        private readonly FarmhouseA _fhA;
+
+        public Overseer(FarmhouseA fha)
+        {
+            if(fha == null)
+            {
+                throw new ArgumentNullException(nameof(fha));
+            }
+            _fhA = fha;
+        }
+
+        public void UpdateView(float frametime)
+        {
+            Log.Info("farmhouse A current level is " + _fhA._level.ToString());
+        }
+    }
+    
+}
